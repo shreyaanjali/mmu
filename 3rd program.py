a91fe98ff92706c5a65b54fc585ab75ecadc5819
shreyaanjali <30632343+shreyaanjali@users.noreply.github.com>
2017-09-17 15:57:17 +0530

    Update and rename 3rd program.py to main.py

--- 3rd program.py
+++
@@ -1,7 +0,0 @@
-page= "<a href=www.facebook.com> facebook </a>"
-start_link="page.find[<a href]"
-start_quote="page.find[www,start_link]"
-end_quote="page.find[com,start_quote+1]"
-url="page start_quote+1:end_quote"
-print
-
